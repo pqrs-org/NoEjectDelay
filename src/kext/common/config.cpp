85245fe3d3d51d3c7f34545fa9da98ca8235224b
Takayama Fumihiko <tekezo@pqrs.org>
2008-05-07 21:59:03 +0900

    change sysctl entry name

--- src/kext/common/config.cpp
+++ src/kext/common/config.cpp
@@ -8,23 +8,23 @@ namespace org_pqrs_NoEjectDelay {
   // ----------------------------------------------------------------------
   // SYSCTL staff
   // http://developer.apple.com/documentation/Darwin/Conceptual/KernelProgramming/boundaries/chapter_14_section_7.html#//apple_ref/doc/uid/TP30000905-CH217-TPXREF116
-  SYSCTL_DECL(_setejectdelay);
-  SYSCTL_NODE(, OID_AUTO, setejectdelay, CTLFLAG_RW, 0, "");
+  SYSCTL_DECL(_noejectdelay);
+  SYSCTL_NODE(, OID_AUTO, noejectdelay, CTLFLAG_RW, 0, "");
 
-  SYSCTL_STRING(_setejectdelay, OID_AUTO, version, CTLFLAG_RD, config_version, 0, "");
+  SYSCTL_STRING(_noejectdelay, OID_AUTO, version, CTLFLAG_RD, config_version, 0, "");
 
   // ----------------------------------------------------------------------
   void
   sysctl_register(void)
   {
-    sysctl_register_oid(&sysctl__setejectdelay);
-    sysctl_register_oid(&sysctl__setejectdelay_version);
+    sysctl_register_oid(&sysctl__noejectdelay);
+    sysctl_register_oid(&sysctl__noejectdelay_version);
   }
 
   void
   sysctl_unregister(void)
   {
-    sysctl_unregister_oid(&sysctl__setejectdelay);
-    sysctl_unregister_oid(&sysctl__setejectdelay_version);
+    sysctl_unregister_oid(&sysctl__noejectdelay);
+    sysctl_unregister_oid(&sysctl__noejectdelay_version);
   }
 }
