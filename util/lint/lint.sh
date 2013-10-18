427b6085d69811d8d22adf04c83ba755c92594f4
Takayama Fumihiko <tekezo@pqrs.org>
2013-10-19 03:04:37 +0900

    update lint

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -5,7 +5,7 @@ basedir=`dirname $0`
 for f in `find $basedir/../../* -name 'project.pbxproj'`; do
     echo "Check $f"
 
-    if [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.7" ]; then
+    if [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.8" ]; then
         "$basedir/xcodeproj.rb" SDKROOT < $f || exit 1
     else
         "$basedir/xcodeproj.rb" < $f || exit 1
