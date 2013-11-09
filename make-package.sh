27a87921e8e23d359c356b35882471c2a4f027eb
Takayama Fumihiko <tekezo@pqrs.org>
2013-11-10 03:17:01 +0900

    PackageMaker -> pkgbuild & productbuild

--- make-package.sh
+++ make-package.sh
@@ -4,10 +4,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 
 version=$(cat version)
 
-packagemaker=/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker
-pkgName="NoEjectDelay.pkg"
-archiveName="NoEjectDelay-${version}"
-
 make clean build || exit $?
 
 # --------------------------------------------------
@@ -31,14 +27,13 @@ done
 cp -R files/scripts "pkgroot/$basedir"
 
 mkdir -p                            "pkgroot/$basedir/extra"
-cp -R pkginfo/Resources/preflight   "pkgroot/$basedir/extra/uninstall.sh"
+cp -R pkginfo/Scripts/preinstall    "pkgroot/$basedir/extra/uninstall.sh"
 cp -R files/extra/setpermissions.sh "pkgroot/$basedir/extra/"
 
 mkdir -p                  "pkgroot/Library"
 cp -R files/LaunchDaemons "pkgroot/Library"
 
 # Sign with Developer ID
-bash files/extra/codesign-kext.sh pkgroot
 bash files/extra/codesign.sh pkgroot
 
 # Setting file permissions.
@@ -52,41 +47,48 @@ bash files/extra/codesign.sh pkgroot
 #     the directory permission will be 0777 in Archive.bom
 #     even if we set this directory permission to 0755 by setpermissions.sh.
 #
-#   Then, we need to repair file permissions in postflight script.
-#   Please also see postflight.
+#   Then, we need to repair file permissions in postinstall script.
+#   Please also see postinstall.
 #
 sh "files/extra/setpermissions.sh" pkgroot
 sh "files/extra/setpermissions.sh" pkginfo
 chmod 755 \
-    pkginfo/Resources/InstallationCheck \
-    pkginfo/Resources/postflight \
-    pkginfo/Resources/preflight \
+    pkginfo/Scripts/postinstall \
+    pkginfo/Scripts/preinstall \
     pkginfo/fixbom.rb
 
 # --------------------------------------------------
-echo "Exec PackageMaker"
+echo "Create pkg"
 
-rm -rf $archiveName/$pkgName
+pkgName="NoEjectDelay.pkg"
+pkgIdentifier="org.pqrs.driver.NoEjectDelay"
+archiveName="NoEjectDelay-${version}"
+
+rm -rf $archiveName
 mkdir $archiveName
 
-# Note: Don't add --no-recommend option.
-# It might break /Library permission.
-# (It overwrites /Library permission with pkgroot/Library permission.)
-# - Mac OS X 10.6: /Library is 1775
-# - Mac OS X 10.7: /Library is 0755
-# - Mac OS X 10.8: /Library is 40755
-$packagemaker \
+pkgbuild \
     --root pkgroot \
-    --info pkginfo/Info.plist \
-    --resources pkginfo/Resources \
-    --title "NoEjectDelay $version" \
-    --no-relocate \
-    --discard-forks \
-    --out $archiveName/$pkgName
+    --component-plist pkginfo/pkgbuild.plist \
+    --scripts pkginfo/Scripts \
+    --identifier $pkgIdentifier \
+    --version $version \
+    --install-location "/" \
+    $archiveName/Installer.pkg
 
-# --------------------------------------------------
 echo "Fix Archive.bom"
-ruby pkginfo/fixbom.rb $archiveName/$pkgName/Contents/Archive.bom pkgroot/
+pkgutil --expand $archiveName/Installer.pkg $archiveName/expanded
+ruby pkginfo/fixbom.rb $archiveName/expanded/Bom pkgroot/
+pkgutil --flatten $archiveName/expanded $archiveName/Installer.pkg
+rm -r $archiveName/expanded
+
+
+productbuild \
+    --distribution pkginfo/Distribution.xml \
+    --package-path $archiveName \
+    $archiveName/$pkgName
+
+rm -f $archiveName/Installer.pkg
 
 # --------------------------------------------------
 echo "Sign with Developer ID"
