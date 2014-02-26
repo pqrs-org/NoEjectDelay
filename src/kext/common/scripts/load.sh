98ad689f3ba4531e345e8eb753886c261e53de66
Takayama Fumihiko <tekezo@pqrs.org>
2014-02-26 14:39:10 +0900

    apply codesign

--- src/kext/common/scripts/load.sh
+++ src/kext/common/scripts/load.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sudo cp -R build/Release/NoEjectDelay.kext /tmp/
-sudo kextutil -t /tmp/NoEjectDelay.kext
+PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+
+# --------------------------------------------------
+sudo rm -rf /tmp/NoEjectDelay
+mkdir /tmp/NoEjectDelay
+
+sudo cp -R build/Release/NoEjectDelay.kext /tmp/NoEjectDelay/NoEjectDelay.signed.kext
+bash ../../../files/extra/codesign.sh /tmp/NoEjectDelay
+sudo chown -R root:wheel /tmp/NoEjectDelay
+
+sudo kextutil -t /tmp/NoEjectDelay/NoEjectDelay.signed.kext
