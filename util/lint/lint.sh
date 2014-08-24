7edf41bd349d1399204750ff7859823e56d2731d
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-25 00:26:17 +0900

    cleanup lint.sh

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -5,9 +5,5 @@ basedir=`dirname $0`
 for f in `find $basedir/../../* -name 'project.pbxproj' ! -ipath '*/Pods/*'`; do
     echo "Check $f"
 
-    if [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.8" ]; then
-        plutil -convert json -o - "$f" | "$basedir/xcodeproj.rb" SDKROOT || exit 1
-    else
-        plutil -convert json -o - "$f" | "$basedir/xcodeproj.rb" || exit 1
-    fi
+    plutil -convert json -o - "$f" | "$basedir/xcodeproj.rb" || exit 1
 done
