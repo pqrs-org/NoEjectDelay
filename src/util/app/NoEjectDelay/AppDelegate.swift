2433c6b911ba0fa1631102c49fda5101fc5c0326
Takayama Fumihiko <tekezo@pqrs.org>
2015-09-21 23:10:02 +0900

    add src/util/app

---
+++ src/util/app/NoEjectDelay/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NoEjectDelay
+//
+//  Created by Takayama Fumihiko on 9/21/15.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
