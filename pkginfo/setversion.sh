3d1d00a56917cb55c66bfda3de8b88fc612b9a00
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-03 02:27:32 +0900

    do not replace Info.plist if not needed

--- pkginfo/setversion.sh
+++ pkginfo/setversion.sh
@@ -4,8 +4,17 @@ basedir=`dirname $0`
 version=$(cat "$basedir/../version")
 
 find $basedir/.. \( -name 'Info.plist.tmpl' -or -name 'Distribution.xml.tmpl' \) -print0 | while IFS= read -r -d '' f; do
-  echo $f
-  if [ -f "$f" ]; then
-    sed "s|PKGVERSION|$version|g" "$f" > `dirname "$f"`/`basename "$f" .tmpl`
-  fi
+    echo $f
+    if [ -f "$f" ]; then
+        outputfile=`dirname $f`/`basename $f .tmpl`
+        tmpfile=`mktemp /tmp/Info.plist.XXXXXX`
+
+        sed "s|PKGVERSION|$version|g" $f > $tmpfile
+        if cmp -s "$tmpfile" "$outputfile"; then
+            # tmpfile and outputfile are same. remove tmpfile.
+            rm -f "$tmpfile"
+        else
+            mv "$tmpfile" "$outputfile"
+        fi
+    fi
 done
