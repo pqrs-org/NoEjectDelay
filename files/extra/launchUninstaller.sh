c726cc08f5858630aa28e75a61aea2f2a8e16e60
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-02 19:40:41 +0900

    remove launchUninstaller.sh

--- files/extra/launchUninstaller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TMPDIR=`/usr/bin/mktemp -d /tmp/NoEjectDelay_uninstaller.XXXXXX` || exit 1
-/usr/bin/rsync -a /Applications/NoEjectDelay.app/Contents/Applications/NoEjectDelayUninstaller.app "$TMPDIR"
-sh '/Library/Application Support/org.pqrs/NoEjectDelay/setpermissions.sh' "$TMPDIR/NoEjectDelayUninstaller.app"
-/usr/bin/open "$TMPDIR/NoEjectDelayUninstaller.app"
