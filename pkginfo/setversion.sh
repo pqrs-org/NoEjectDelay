cc2f3236a9573c5c531ee55e92fad5c626433aea
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-03 02:31:54 +0900

    fix setversion.sh

--- pkginfo/setversion.sh
+++ pkginfo/setversion.sh
@@ -4,12 +4,11 @@ basedir=`dirname $0`
 version=$(cat "$basedir/../version")
 
 find $basedir/.. \( -name 'Info.plist.tmpl' -or -name 'Distribution.xml.tmpl' \) -print0 | while IFS= read -r -d '' f; do
-    echo $f
     if [ -f "$f" ]; then
-        outputfile=`dirname $f`/`basename $f .tmpl`
+        outputfile=`dirname "$f"`/`basename "$f" .tmpl`
         tmpfile=`mktemp /tmp/Info.plist.XXXXXX`
 
-        sed "s|PKGVERSION|$version|g" $f > $tmpfile
+        sed "s|PKGVERSION|$version|g" "$f" > $tmpfile
         if cmp -s "$tmpfile" "$outputfile"; then
             # tmpfile and outputfile are same. remove tmpfile.
             rm -f "$tmpfile"
